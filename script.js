185fa39427ecac44bea0fe4e1dd32147348bdf9d
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-21 15:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let count=0;
 let ele=document.getElementById("counter");
 ele.innerText=count;
+let btn=document.getElementById("incrementBtn");
+btn.addEventListener("click",IncrementCounter);
 function IncrementCounter(){
 	alert(ele.innerText)
 	ele.innerText=++count;
