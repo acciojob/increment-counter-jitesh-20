7af6ae57475c7800a348d43f61e17195ca6c7405
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-21 15:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count=0;
+let ele=document.getElementById("counter");
+ele.innerText=count;
+function IncrementCounter(){
+	alert(ele.innerText)
+	ele.innerText=++count;
+}
