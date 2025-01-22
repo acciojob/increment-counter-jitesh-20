a95749c6f0e48158c679acfbd62a404c95efe908
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-22 09:33:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 let count=0;
 let ele=document.getElementById("counter");
 ele.innerText=count;
-document.getElementById("incrementBtn").click(){
-	alert(ele.innerText)
-	ele.innerText=++count;
-}
-// btn.addEventListener("click",IncrementCounter);
-// function IncrementCounter(){
+// document.getElementById("incrementBtn").click(){
 // 	alert(ele.innerText)
 // 	ele.innerText=++count;
 // }
+const btn=document.getElementById("incrementBtn");
+btn.addEventListener("click",IncrementCounter);
+function IncrementCounter(){
+	alert(ele.innerText)
+	ele.innerText=++count;
+}
